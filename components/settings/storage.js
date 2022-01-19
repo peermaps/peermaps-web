7ddbdf95a1fa499cb5e4e36c8b908a10890e0597
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-01-19 14:03:19 +0100

    Add textarea to storage backends + tweak alignments

--- components/settings/storage.js
+++ components/settings/storage.js
@@ -6,7 +6,6 @@ var backendStyle = css`
     padding: 10px;
     padding-right: 15px;
     border-bottom: 1px solid #999;
-    margin-bottom: 10px;
   }
 `
 
@@ -32,6 +31,12 @@ function StorageTab () {
         var data = settings.getTabData('storage')
         var backend = data.backends[index]
         backend.zoom.max = Math.max(Number(max), backend.zoom.min)
+        emitter.emit('settings:dirty')
+      })
+      emitter.on('settings:storage:description:update', function (index, description) {
+        var data = settings.getTabData('storage')
+        var backend = data.backends[index]
+        backend.description = description
         emitter.emit('settings:dirty')
       })
       emitter.on('settings:storage:active:update', function (index) {
@@ -64,13 +69,15 @@ function StorageTab () {
           <input type='range' name='minzoom' min='1' max='21' step='1' value=${zoom.min} style='width: 100%;' onchange=${(e) => emit('settings:storage:minzoom:update', index, e.target.value)}>
           <label for='maxzoom'>max zoom level (${zoom.max})</label>
           <input type='range' name='maxzoom' min='1' max='21' step='1' value=${zoom.max} style='width: 100%;' onchange=${(e) => emit('settings:storage:maxzoom:update', index, e.target.value)}>
+          <label for='active'>description</label>
+          <textarea name='description' style='resize: none; height: 5em; width: 100%' onchange=${(e) => emit('settings:storage:description:update', index, e.target.value)}>${item.description}</textarea>
           <label for='active'>active</label>
           <input type='checkbox' name='active' style='margin-left: 10px;' onchange=${(e) => emit('settings:storage:active:update', index)} ${item.active ? 'checked' : ''} value=${item.active ? true : false}>
         </div>`
       })
       return html`<div>
         ${content}
-        <div style='position: absolute; left: 10px; cursor: pointer; padding-left: 4px; padding-right: 4px; border: 1px solid #999' onclick=${() => emit('settings:storage:add')}>+</div>
+        <div style='cursor: pointer; margin: 5px; padding: 2px; border: 1px solid #999; width: 14px; text-align: center;' onclick=${() => emit('settings:storage:add')}>+</div>
       </div>`
     },
     defaultData: function () {
@@ -79,16 +86,19 @@ function StorageTab () {
           // TODO use array range for zoom: [1, 21]
           {
             url: 'https://peermaps.linkping.org',
+            description: 'Peermaps data hosted by linkping.org',
             zoom: { min: 1, max: 21 },
             active: true
           },
           {
             url: 'https://ipfs.io/ipfs/QmVCYUK51Miz4jEjJxCq3bA6dfq5FXD6s2EYp6LjHQhGmh',
+            description: 'Dataset on ipfs',
             zoom: { min: 1, max: 21 },
             active: false
           },
           {
             url: 'http://localhost:8000',
+            description: 'Data from local machine',
             zoom: { min: 1, max: 21 },
             active: false
           }
