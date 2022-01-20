1281d2ad6a71892a51e8f3df3d8569c95cf17331
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-01-20 22:52:35 +0100

    Use indexhtmlify instead

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>peermaps</title><meta charset="utf-8"></head><body><script src="app.js"></script></body></html>' > public/index.html
-browserify app.js > public/app.js
