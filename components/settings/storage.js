dd2a27e6f2f4b79f83ccb1765f7d40c867b15219
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-01-20 12:48:58 +0100

    Use array for zoom range values

--- components/settings/storage.js
+++ components/settings/storage.js
@@ -24,13 +24,13 @@ function StorageTab () {
       emitter.on('settings:storage:minzoom:update', function (index, min) {
         var data = settings.getTabData('storage')
         var backend = data.backends[index]
-        backend.zoom.min = Math.min(Number(min), backend.zoom.max)
+        backend.zoom[0] = Math.min(Number(min), backend.zoom[1])
         emitter.emit('settings:dirty')
       })
       emitter.on('settings:storage:maxzoom:update', function (index, max) {
         var data = settings.getTabData('storage')
         var backend = data.backends[index]
-        backend.zoom.max = Math.max(Number(max), backend.zoom.min)
+        backend.zoom[1] = Math.max(Number(max), backend.zoom[0])
         emitter.emit('settings:dirty')
       })
       emitter.on('settings:storage:description:update', function (index, description) {
@@ -52,7 +52,7 @@ function StorageTab () {
       })
       emitter.on('settings:storage:add', function () {
         var data = settings.getTabData('storage')
-        data.backends.push({ zoom: { min: 1, max: 21 }, active: false })
+        data.backends.push({ zoom: [1, 21], active: false })
         emitter.emit('settings:dirty')
       })
     },
@@ -65,10 +65,10 @@ function StorageTab () {
           <div style='position: absolute; right: 10px; cursor: pointer; padding-left: 4px; padding-right: 4px; border: 1px solid #999' onclick=${() => emit('settings:storage:delete', index)}>X</div>
           <label for='url'>data url</label>
           <input type='url' name='url' value=${item.url || ''} placeholder='https://example.com' required style='margin-top: 10px; margin-bottom: 10px; width: 100%;' onchange=${(e) => emit('settings:storage:url:update', index, e.target.value)}>
-          <label for='minzoom'>min zoom level (${zoom.min})</label>
-          <input type='range' name='minzoom' min='1' max='21' step='1' value=${zoom.min} style='width: 100%;' onchange=${(e) => emit('settings:storage:minzoom:update', index, e.target.value)}>
-          <label for='maxzoom'>max zoom level (${zoom.max})</label>
-          <input type='range' name='maxzoom' min='1' max='21' step='1' value=${zoom.max} style='width: 100%;' onchange=${(e) => emit('settings:storage:maxzoom:update', index, e.target.value)}>
+          <label for='minzoom'>min zoom level (${zoom[0]})</label>
+          <input type='range' name='minzoom' min='1' max='21' step='1' value=${zoom[0]} style='width: 100%;' onchange=${(e) => emit('settings:storage:minzoom:update', index, e.target.value)}>
+          <label for='maxzoom'>max zoom level (${zoom[1]})</label>
+          <input type='range' name='maxzoom' min='1' max='21' step='1' value=${zoom[1]} style='width: 100%;' onchange=${(e) => emit('settings:storage:maxzoom:update', index, e.target.value)}>
           <label for='active'>description</label>
           <textarea name='description' style='resize: none; height: 5em; width: 100%' onchange=${(e) => emit('settings:storage:description:update', index, e.target.value)}>${item.description}</textarea>
           <label for='active'>active</label>
@@ -83,23 +83,22 @@ function StorageTab () {
     defaultData: function () {
       return {
         backends: [
-          // TODO use array range for zoom: [1, 21]
           {
             url: 'https://peermaps.linkping.org',
             description: 'Peermaps data hosted by linkping.org',
-            zoom: { min: 1, max: 21 },
+            zoom: [1, 21],
             active: true
           },
           {
             url: 'https://ipfs.io/ipfs/QmVCYUK51Miz4jEjJxCq3bA6dfq5FXD6s2EYp6LjHQhGmh',
             description: 'Dataset on ipfs',
-            zoom: { min: 1, max: 21 },
+            zoom: [1, 21],
             active: false
           },
           {
             url: 'http://localhost:8000',
             description: 'Data from local machine',
-            zoom: { min: 1, max: 21 },
+            zoom: [1, 21],
             active: false
           }
         ]
