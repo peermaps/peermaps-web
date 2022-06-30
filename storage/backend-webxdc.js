32ef50ae03e7184dcf0c46828cd8a40caa1a5c33
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-06-30 16:55:10 +0200

    Fix webxdc backend to read data from cache.js

--- storage/backend-webxdc.js
+++ storage/backend-webxdc.js
@@ -1,7 +1,32 @@
+const cache = require('./cache')
+const nextTick = process.nextTick
+
 function createStorageBackend (state, url) {
-  // var protocol = typeof url === 'string' ? url.split('://')[0] : ''
-  // TODO create some kind of backend for webxdc, must follow the same
-  // api as for http and hyperdrive
+  return {
+    length: function f (name, cb) {
+      var data = cache[name]
+      if (data) {
+        nextTick(cb, null, data.length)
+      } else {
+        nextTick(cb, new Error(`could not find tree file ${name}`))
+      }
+    },
+    read: function f (name, offset, length, cb) {
+      var data = cache[name]
+      if (data) {
+        if (offset === 0 && length === data.length) {
+          nextTick(cb, null, data)
+        } else {
+          nextTick(cb, null, data.subarray(offset, offset+length))
+        }
+      } else {
+        nextTick(cb, new Error(`could not find tree file ${name}`))
+      }
+    },
+    getRootUrl: function () { return '' },
+    setRootUrl: function (url) {},
+    destroy: function (name, cb) { nextTick(cb) }
+  }
 }
 
 module.exports = createStorageBackend
