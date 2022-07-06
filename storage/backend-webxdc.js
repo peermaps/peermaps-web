1da1d568f2d64c1d70d976602aa21a053f5cb6d1
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-07-06 11:58:04 +0200

    use http backend for webxdc and remove storage/map-cache.js

--- storage/backend-webxdc.js
+++ storage/backend-webxdc.js
@@ -1,32 +1,10 @@
-const cache = require('./map-cache')
-const nextTick = process.nextTick
+var createHttpBackend = require('./http')
 
 function createStorageBackend (state, url) {
-  return {
-    length: function f (name, cb) {
-      var data = cache[name]
-      if (data) {
-        nextTick(cb, null, data.length)
-      } else {
-        nextTick(cb, new Error(`could not find tree file ${name}`))
-      }
-    },
-    read: function f (name, offset, length, cb) {
-      var data = cache[name]
-      if (data) {
-        if (offset === 0 && length === data.length) {
-          nextTick(cb, null, data)
-        } else {
-          nextTick(cb, null, data.subarray(offset, offset+length))
-        }
-      } else {
-        nextTick(cb, new Error(`could not find tree file ${name}`))
-      }
-    },
-    getRootUrl: function () { return '' },
-    setRootUrl: function (url) {},
-    destroy: function (name, cb) { nextTick(cb) }
-  }
+  // TODO using http backend directly for all urls, even though only ipfs
+  // is supported over http (since webxdc client can't do http calls to
+  // http urls on the outside)
+  return createHttpBackend(url)
 }
 
 module.exports = createStorageBackend
