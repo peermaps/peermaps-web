eb145bba62a2fd9eed6c496e1151137868294979
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-07-24 01:41:52 +0200

    tweak padding and width of endpoint div

--- view/settings/tabs/storage.js
+++ view/settings/tabs/storage.js
@@ -4,7 +4,6 @@ var css = require('sheetify')
 var endpointStyle = css`
   :host {
     padding: 10px;
-    padding-right: 15px;
     border-bottom: 1px solid #999;
   }
 `
@@ -35,13 +34,13 @@ module.exports = function (state, emit) {
 
     return html`<div class=${endpointStyle}>
       <label for='url'>${l('storage_tab_url')}</label>
-      <input type='url' name='url' value=${endpoint.url || ''} placeholder='https://example.com' required style='margin-top: 10px; margin-bottom: 10px; width: 100%;' onchange=${(e) => emit('settings:storage:url:update', index, e.target.value)}>
+      <input type='url' name='url' value=${endpoint.url || ''} placeholder='https://example.com' required style='margin-top: 10px; margin-bottom: 10px; width: calc(100% - 1ex);' onchange=${(e) => emit('settings:storage:url:update', index, e.target.value)}>
       <label for='minzoom'>${l('storage_tab_min_zoom')} (${zoom[0]})</label>
       <input type='range' name='minzoom' min='1' max='21' step='1' value=${zoom[0]} style='width: 100%;' onchange=${(e) => emit('settings:storage:minzoom:update', index, e.target.value)}>
       <label for='maxzoom'>${l('storage_tab_max_zoom')} (${zoom[1]})</label>
       <input type='range' name='maxzoom' min='1' max='21' step='1' value=${zoom[1]} style='width: 100%;' onchange=${(e) => emit('settings:storage:maxzoom:update', index, e.target.value)}>
       <label for='active'>${l('storage_tab_description')}</label>
-      <textarea name='description' style='resize: none; height: 5em; width: 100%' onchange=${(e) => emit('settings:storage:description:update', index, e.target.value)}>${endpoint.description}</textarea>
+      <textarea name='description' style='resize: none; height: 5em; width: calc(100% - 1ex);' onchange=${(e) => emit('settings:storage:description:update', index, e.target.value)}>${endpoint.description}</textarea>
       <label for='active'>${l('storage_tab_active')}</label>
       <input type='checkbox' name='active' style='margin-left: 10px; margin-bottom: 10px;' onchange=${(e) => emit('settings:storage:active:update', index)} ${endpoint.active ? 'checked' : ''} value=${endpoint.active ? true : false}>
       <div style='display: flex; justify-content: space-between;'>
