c103971e79b434c6ccdc029b00ca97fdefe188e7
Lars-Magnus Skog <ralphtheninja@riseup.net>
2022-07-24 02:15:12 +0200

    add select for locale in ui tab

--- store/settings/tabs/ui.js
+++ store/settings/tabs/ui.js
@@ -1,11 +1,23 @@
 var i18n = require('../../../lib/i18n.js')
 
 module.exports = function (state, emitter) {
+  // TODO get locale value from db
   var locale = 'en-US'
-  // var locale = 'sv-SE'
   state.settings.ui = {
+    get locale () {
+      return locale
+    },
+    locales: [
+      { value: 'en-US', description: 'english - american' },
+      { value: 'sv-SE', description: 'svenska' },
+    ],
     lookup: function (key) {
       return i18n[locale][key] || i18n['en-US'][key] || 'n/a'
     }
   }
+
+  emitter.on('settings:ui:locale:update', function (value) {
+    locale = value
+    emitter.emit('settings:dirty')
+  })
 }
